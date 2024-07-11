de9619043e4d4c2171b207fda85d508f41dd230d
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-07-11 23:28:50 +0530

    button added input

---
+++ src/components/Button.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Button({
+  children,
+  type = "button",
+  bgColor = "bg-blue-600",
+  textColor = "text-white",
+  className = "",
+  onclick,
+  ...props
+}) {
+  return (
+    <button
+      className={`px-[1.8rem] py-[0.65rem] rounded-lg bg-primary ${bgColor} ${textColor} ${className} hover:scale-95 hover:bg-primaryDark transition duration-300 font-medium`}
+      {...props}
+      // onClick={onclick}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
