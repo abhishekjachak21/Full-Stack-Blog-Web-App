5b2af06d8fbca9f9cf56609ee3b1fa9bbad25b03
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-07-15 20:52:48 +0530

    session changed

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -4,8 +4,8 @@ import { useNavigate } from "react-router-dom";
 
 export default function Protected({ children, authentication = true }) {
   const navigate = useNavigate();
-  // const [loader, setLoader] = useState(true);
   const authStatus = useSelector((state) => state.auth.status);
+// const [loader, setLoader] = useState(true);
 
   useEffect(() => {
     //TODO: make it more easy to understand
