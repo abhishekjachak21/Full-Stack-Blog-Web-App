d1d196a2989407f69a6e24f767ab3139bca5dea8
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-07-17 01:04:25 +0530

    addpost gets up

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, PostForm } from "../components/index";
+
 function AddPost() {
   return (
     <div className="py-8 bg-bgLight flex items-center justify-center">
